c9b9114f0a001be0e51d23ca39de0bf7c384de38
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-06 14:22:14 -0400

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Lori Rothermel on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
